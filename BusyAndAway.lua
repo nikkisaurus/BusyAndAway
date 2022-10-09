8d88efa5e5ac3e3ceac7cdb42b18140cdbd8eec5
Jasmine <jlvellocido@gmail.com>
2022-10-09 02:59:02 -0500

    Rewrite

--- BusyAndAway.lua
+++ BusyAndAway.lua
@@ -1,6 +1,13 @@
 local addonName, private = ...
 local BusyAndAway = LibStub("AceAddon-3.0"):GetAddon(addonName)
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName, true)
+local AceGUI = LibStub("AceGUI-3.0")
+
+local whispers
+local chatColors = {
+	CHAT_MSG_WHISPER = "FFFF7EFF",
+	CHAT_MSG_BN_WHISPER = "FF00FAF6",
+}
 
 --[[ Init ]]
 function BusyAndAway:OnInitialize()
@@ -31,6 +38,8 @@ function BusyAndAway:OnInitialize()
 			},
 		},
 	}, true)
+
+	BusyAndAway:SetEnabledState(private.db.global.enabled)
 end
 
 --[[ OnEnable/OnDisable ]]
@@ -49,7 +58,7 @@ end
 --[[ Slash command ]]
 function BusyAndAway:HandleSlashCommand(input)
 	local cmd, args = strmatch(input, "(%w+)%s*(.*)")
-	cmd = strlower(cmd)
+	cmd = cmd and strlower(cmd)
 
 	if cmd == "start" then
 		BusyAndAway:StartSession(args)
@@ -57,16 +66,20 @@ function BusyAndAway:HandleSlashCommand(input)
 		BusyAndAway:EndSession()
 	elseif cmd == "delay" then
 		private:SetDelay(args)
+	else
+		private:LoadOptions()
 	end
 end
 
 function BusyAndAway:StartSession(msg)
 	private.db.global.session.active = true
 	private.db.global.session.status = msg
-	BusyAndAway:Print(format("%s: %s", L["Session started"], msg))
+	whispers = nil
+	BusyAndAway:Print(format("%s%s %s", L["Session started"], msg and msg ~= "" and ":" or "", msg))
 end
 
 function BusyAndAway:EndSession()
+	whispers = private.db.global.session.whispers
 	private.db.global.session = private.defaultSession
 	BusyAndAway:Print(
 		format(
@@ -81,9 +94,25 @@ function private:ViewChats(...)
 	local _, identifier = strsplit(":", ...)
 
 	if identifier == addonName then
-		-- Do whatever you want.
-		for _, message in pairs(private.db.global.session.whispers) do
-			print(unpack(message))
+		-- Create summary frame
+		local summary = private.summary or AceGUI:Create("Window")
+		private.summary = summary
+		summary:SetTitle(format("%s %s", L.addonName, L["Session Whispers"]))
+		summary:SetWidth(400)
+		summary:SetHeight(300)
+		summary:Show()
+		summary:ReleaseChildren()
+
+		-- Add messages to summary
+		for _, message in pairs(whispers) do
+			local Type, Time, msg, friend = unpack(message)
+			local color = CreateColorFromHexString(chatColors[Type])
+
+			local line = AceGUI:Create("Label")
+			line:SetFullWidth(true)
+			line:SetColor(color:GetRGB())
+			line:SetText(format("[%s] %s: %s", date("%X", Time), friend, msg))
+			summary:AddChild(line)
 		end
 	end
 end
@@ -99,6 +128,9 @@ end
 
 --[[ Core ]]
 function BusyAndAway:PLAYER_ENTERING_WORLD()
+	-- Debug
+	-- private:LoadOptions()
+
 	local session = private.db.global.session
 	if not session.active then
 		return
@@ -122,9 +154,13 @@ function BusyAndAway:CHAT_MSG_WHISPER(event, ...)
 	end
 
 	-- Save chats
-	tinsert(session.whispers, { event, ... })
+	tinsert(session.whispers, { event, time(), ... })
 
 	-- Auto respond
+	if not private.db.global.enabled then
+		return
+	end
+
 	local isBnetMsg = event == "CHAT_MSG_BN_WHISPER"
 	local friend = select(isBnetMsg and 13 or 2, ...)
 	local msg = (session.status and session.status ~= "") and session.status or private.defaultStatus
@@ -150,3 +186,85 @@ end
 BusyAndAway.CHAT_MSG_BN_WHISPER = BusyAndAway.CHAT_MSG_WHISPER
 
 --[[ Config ]]
+function private:LoadOptions()
+	local options = private.options or AceGUI:Create("Frame")
+	private.options = options
+	options:SetTitle(L.addonName)
+	options:SetLayout("Fill")
+	options:SetWidth(400)
+	options:SetHeight(300)
+	options:Show()
+	options:ReleaseChildren()
+
+	local scrollFrame = AceGUI:Create("ScrollFrame")
+	options:AddChild(scrollFrame)
+
+	local heading = AceGUI:Create("Label")
+	heading:SetFullWidth(true)
+	heading:SetFontObject(GameFontNormalLarge)
+	heading:SetColor(1, 0.82, 0)
+	heading:SetText(L["Settings"])
+	scrollFrame:AddChild(heading)
+
+	local enabled = AceGUI:Create("CheckBox")
+	enabled:SetFullWidth(true)
+	enabled:SetLabel(L["Enabled"])
+	enabled:SetValue(private.db.global.enabled)
+	enabled:SetCallback("OnValueChanged", function(_, _, enabled)
+		private.db.global.enabled = enabled
+	end)
+	scrollFrame:AddChild(enabled)
+
+	local whispersHeading = AceGUI:Create("Label")
+	whispersHeading:SetFullWidth(true)
+	whispersHeading:SetColor(1, 0.82, 0)
+	whispersHeading:SetText(L["Respond to:"])
+	scrollFrame:AddChild(whispersHeading)
+
+	local whisper = AceGUI:Create("CheckBox")
+	whisper:SetFullWidth(true)
+	whisper:SetLabel("CHAT_MSG_WHISPER")
+	whisper:SetValue(private.db.global.settings.CHAT_MSG_WHISPER)
+	whisper:SetCallback("OnValueChanged", function(_, _, enabled)
+		private.db.global.settings.CHAT_MSG_WHISPER = enabled
+	end)
+	scrollFrame:AddChild(whisper)
+
+	local bnWhisper = AceGUI:Create("CheckBox")
+	bnWhisper:SetFullWidth(true)
+	bnWhisper:SetLabel("CHAT_MSG_BN_WHISPER")
+	bnWhisper:SetValue(private.db.global.settings.CHAT_MSG_BN_WHISPER)
+	bnWhisper:SetCallback("OnValueChanged", function(_, _, enabled)
+		private.db.global.settings.CHAT_MSG_BN_WHISPER = enabled
+	end)
+	scrollFrame:AddChild(bnWhisper)
+
+	local miscHeading = AceGUI:Create("Label")
+	miscHeading:SetFullWidth(true)
+	miscHeading:SetColor(1, 0.82, 0)
+	miscHeading:SetText(L["Miscellaneous:"])
+	scrollFrame:AddChild(miscHeading)
+
+	local autoResponse = AceGUI:Create("CheckBox")
+	autoResponse:SetFullWidth(true)
+	autoResponse:SetLabel(L["Use [Auto Response] tag in replies"])
+	autoResponse:SetValue(private.db.global.settings.autoResponseTag)
+	autoResponse:SetCallback("OnValueChanged", function(_, _, enabled)
+		private.db.global.settings.autoResponseTag = enabled
+	end)
+	scrollFrame:AddChild(autoResponse)
+
+	local delayLabel = AceGUI:Create("Label")
+	delayLabel:SetFullWidth(true)
+	delayLabel:SetText(L["Delay (in seconds) controls the time between auto responses to the same recipient."])
+	scrollFrame:AddChild(delayLabel)
+
+	local delay = AceGUI:Create("Slider")
+	delay:SetLabel(L["Delay"])
+	delay:SetSliderValues(0, 60 * 30, 1)
+	delay:SetValue(private.db.global.settings.delay)
+	delay:SetCallback("OnValueChanged", function(_, _, delay)
+		private.db.global.settings.delay = delay
+	end)
+	scrollFrame:AddChild(delay)
+end
--- Locales/en-us.lua
+++ Locales/en-us.lua
@@ -1,14 +1,22 @@
 local addonName, private = ...
-local BusyAndAway = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
+local BusyAndAway = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true)
 LibStub("LibAddonUtils-1.0"):Embed(BusyAndAway)
 
-L.addon = "Busy and Away"
+L.addonName = "Busy and Away"
 
 L["Auto Response"] = true
+L["Delay"] = true
+L["Delay (in seconds) controls the time between auto responses to the same recipient."] = true
+L["Enabled"] = true
 L["Message delay set to %d."] = true
+L["Miscellaneous:"] = true
+L["Respond to:"] = true
 L["Session ended."] = true
 L["Session started"] = true
+L["Session Whispers"] = true
+L["Settings"] = true
 L["Sorry, I'm unavailable at the moment."] = true
 L["There is currently an active session."] = true
+L["Use [Auto Response] tag in replies"] = true
 L["View messages"] = true
